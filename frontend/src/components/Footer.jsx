169bed37c5e6fa87fa6dc65559269d79841dda8c
grubor-maja <maja.grubor@outlook.com>
2024-10-24 20:49:27 +0200

    added link to footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from '../assets/psychonautlogo-light.png';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return ( <>
         <footer className="footer">
             <div className="footer-logo">
-                <img src={logo} alt="Logo" />
+                <Link to="/"><img src={logo} alt="Logo" /></Link>
             </div>
             <div className="footer-text">
                 <p>Terms & Conditions | All rights reserved.</p>

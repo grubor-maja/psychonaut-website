4d07b6c079a31350cdfd0f7d8bc21a43c14535a2
grubor-maja <maja.grubor@outlook.com>
2024-10-24 20:17:59 +0200

    add links to navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../assets/psychonautlogo-light.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return ( <>
@@ -8,11 +9,10 @@ function Navbar() {
                 <img src={logo} alt="Psychonaut Logo" />
             </div>
             <ul className="navbar-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#book">Book a Session</a></li>
-                <li><a href="#contact">Contact Us</a></li>
+                <li><Link to="/about-me">About Me</Link></li>
+                <li><Link to="/services">Services</Link></li>
+                <li><Link to="/book">Book a Session</Link></li>
+                <li><Link to="/contact">Contact Us</Link></li>
             </ul>
         </nav>
     </> );

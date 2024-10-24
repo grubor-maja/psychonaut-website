eea4ea4982941a63af8386f2bffee6651c869db3
grubor-maja <maja.grubor@outlook.com>
2024-10-24 11:19:40 +0200

    add css variables for color and typography

---
+++ frontend/src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+function Homepage() {
+    return (
+        <>
+        <p className='text-dark-navy'>Psychonaut</p>
+        </>
+    );
+}
+
+export default Homepage;
+

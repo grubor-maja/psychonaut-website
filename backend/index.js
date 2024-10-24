4c4b844f288ef5382983b1839a9d89f6dfdd4988
grubor-maja <maja.grubor@outlook.com>
2024-10-24 23:29:37 +0200

    add deployment options

--- backend/index.js
+++ backend/index.js
@@ -5,13 +5,22 @@ import sessionRoutes from './routes/freeSession.route.js';
 import connectDB from './configuration/db.js';
 import { FreeSession } from './models/freeSession.model.js';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5006;
+const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 
 connectDB();
 await FreeSession.syncIndexes();

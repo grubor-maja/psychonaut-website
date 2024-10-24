8790eb54cf1891f99f5c16c3754acfaaca05a3dd
grubor-maja <maja.grubor@outlook.com>
2024-10-24 22:39:39 +0200

    fix unused variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import Homepage from './pages/Homepage';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import ImageTextSection from './components/ImageTextSection';
-import HeroSection from './components/HeroSection';
-import ScrollingSection from './components/ScrollingSection';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import About from './pages/About';
 import Services from './pages/Services';

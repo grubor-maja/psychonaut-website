1748e2477e01bd50451c7fa40352aa989f793959
grubor-maja <maja.grubor@outlook.com>
2024-10-31 13:56:35 +0100

    add animation to home page

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -4,7 +4,9 @@ function HeroSection() {
     return ( <>
         <div className="hero-section">
             <div className="hero-content">
-                <h1>Find Clarity, Healing, and Strength</h1>
+                <h1>
+                    Find <span className="animated-word">Clarity</span>, <span className="animated-word">Healing</span>, and <span className="animated-word">Strength</span>
+                </h1>                
                 <p>Offering a safe space to explore your thoughts, emotions, and regain control over your life.</p>
                 <button>Book a Session</button>
             </div>

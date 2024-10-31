95efb68b7f46d6979ea48fa0d8affa45e4929dc9
grubor-maja <maja.grubor@outlook.com>
2024-10-31 13:48:55 +0100

    add burger menu for mobile devices

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,25 @@
-import React, { Component } from 'react';
 import logo from '../assets/psychonautlogo-light.png';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import burgerMenuIcon from '../assets/menu.png';
 
 function Navbar() {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
     return ( <>
         <nav className='navbar'>
-            <div className="navbar-logo">
+            <div className="navbar-logo navbar-mobile">
                 <Link to="/"><img src={logo} alt="Psychonaut Logo" /></Link>
+                <span className="menu-icon" onClick = {toggleMenu}>
+                 <img src={burgerMenuIcon} className='burgermenu-image'/>
+            </span>
             </div>
-            <ul className="navbar-links">
+
+
+            <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
                 <li><Link to="/about-me">About Me</Link></li>
                 <li><Link to="/services">Services</Link></li>
                 <li><Link to="/session">Book a Session</Link></li>
